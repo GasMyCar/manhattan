f2ae8c01829fba510f952c20f47d36838e6f29d4
GasMyCar <jmmognetti@mac.com>
2012-08-07 12:50:35 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Manhattan::Application.config.secret_token = 'd44ed154aa4c32f47a40988dceb58a517fb41c4acaca43948e0b892d72a161986fb4950d0b6f2adeb05d4e464ade1d20ecf8981e2f6ebc68b39d56c9cf1d5b57'
