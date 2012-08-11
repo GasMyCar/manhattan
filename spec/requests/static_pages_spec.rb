33732e1d6eca06e8eb8fcab411e351dc8e9e7c61
GasMyCar <jmmognetti@mac.com>
2012-08-11 13:26:21 +0100

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,17 +2,19 @@ require 'spec_helper'
 
 describe "Static pages" do
   
-  let (:base_title) {"Manhattan Project App"}
   describe "Home page" do
 
     it "should have the h1 'Manhattan Project'" do
       visit '/static_pages/home'
       page.should have_selector('h1', :text => 'Manhattan Project')
     end
-    
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      page.should have_selector('title',                                                                        :text => "#{base_title} | Home")
+      page.should have_selector('title',                                                                        :text => "Manhattan Project App")
+    end
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
 
@@ -25,7 +27,7 @@ describe "Static pages" do
     it "should have the title 'Help'" do
       visit '/static_pages/help'
       page.should have_selector('title',
-                       :text => "#{base_title} | Help")
+                       :text => "Manhattan Project App | Help")
     end
   end
   
@@ -38,7 +40,7 @@ describe "Static pages" do
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
       page.should have_selector('title',
-                       :text => "#{base_title} | About Us")
+                       :text => "Manhattan Project App | About Us")
     end
   end
  
@@ -51,7 +53,7 @@ describe "Static pages" do
     it "should have the title 'Contact Us'" do
       visit '/static_pages/contact'
       page.should have_selector('title',
-                       :text => "#{base_title} | Contact Us")
+                       :text => "Manhattan Project App | Contact Us")
     end
   end 
 end 
